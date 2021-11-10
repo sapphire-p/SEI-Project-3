6e89fd9abe50580ee0f9359c72807bef9a463b37
Isaac Franks <iglfranks@outlook.com>
2021-11-10 13:21:42 +0000

    finished secure route

--- config/router.js
+++ config/router.js
@@ -6,7 +6,6 @@ const router = express.Router()
 router.route('/museums')
   .get(getAllMuseums)
 
-
 router.route('/museums/:id')
   .get(getSingleMuseum)
 
---
+++ config/secureRoute.js
@@ -0,0 +1,23 @@
+import User from '../models/user.js'
+import jwt from 'jsonwebtoken'
+import { secret } from './environment.js'
+
+export const secureRoute = async (req, res, next) => {
+  try {
+    // check if theres a token on the incoming request
+    if (!req.headers.authorization) throw new Error()
+    // extract the token from request
+    const token = req.headers.authorization.replace('Bearer ', '')
+    // get the user info from token
+    const payload = jwt.verify(token, secret)
+    //find the user in the db to check they exist
+    const userToVerify = await User.findById(payload.sub)
+    console.log('USER TO VERIFY ->', userToVerify)
+    // otherwise unauthorized
+    if (!userToVerify) throw new Error()
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+}

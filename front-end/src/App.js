91d0d31bf74990b9f9fad8b6b64d4c3622a9d6c8
Sapphire Paston <sapphire.paston@gmail.com>
2021-11-11 21:24:49 +0000

    FilterPanel componentbuilt and passing formData via a Link to '/filterresults'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import MuseumShow from './components/MuseumShow'
 import Login from './components/Login'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import FilterPanel from './components/FilterPanel'
 
 const App = () => {
 
@@ -15,10 +16,11 @@ const App = () => {
       <NavBar />
       <Switch>
         <Route exact path='/register' component={Register} />
-        <Route exact path='/museums' component={MuseumsIndex}/>
-        <Route exact path='/museums/:id' component={MuseumShow}/>
+        <Route exact path='/museums' component={MuseumsIndex} />
+        <Route exact path='/museums/:id' component={MuseumShow} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/' component={Home} />
+        <Route exact path='/filterpanel' component={FilterPanel} />
 
       </Switch>
     </BrowserRouter>

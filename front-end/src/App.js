f263d95b896e65a018f080843000de754b0dcdfe
Sapphire Paston <sapphire.paston@gmail.com>
2021-11-11 11:11:49 +0000

    App.js set up, ready to split off and create separate features

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,49 +1,17 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useHistory } from 'react-router-dom'
-// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Register from './components/Register'
 
 const App = () => {
 
-  const history = useHistory()
-
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirmation: ''
-  })
-
-  const [errors, setErrors] = useState({
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirmation: ''
-  })
-
-
 
   return (
-    <section className='section'>
-      <div className='container'>
-        <div className='columns'>
-          <form className='column is-half is-offset-one-quarter box'>
-            <div className='control'>
-              <input
-                className={`input ${errors.username ? 'is-danger' : ''}`}
-                placeholder='Username'
-                name='username'
-                value={formData.username}
-              // onChange
-              />
-            </div>
-            <div className='field'>
-              <label className='label'>Username</label>
-            </div>
-          </form>
-        </div>
-      </div>
-    </section>
+    <BrowserRouter>
+      {/* <NavBar /> */}
+      <Switch>
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 

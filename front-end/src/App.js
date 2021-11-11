649fcf3e8a2d84d83fece7c80b7082d2ed858342
Sapphire Paston <sapphire.paston@gmail.com>
2021-11-11 10:53:02 +0000

    Frontend connected, started login frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
 
+  const history = useHistory()
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+
+  const [errors, setErrors] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+
+
+
   return (
-    <h1> Hello World</h1>
+    <section className='section'>
+      <div className='container'>
+        <div className='columns'>
+          <form className='column is-half is-offset-one-quarter box'>
+            <div className='control'>
+              <input
+                className={`input ${errors.username ? 'is-danger' : ''}`}
+                placeholder='Username'
+                name='username'
+                value={formData.username}
+              // onChange
+              />
+            </div>
+            <div className='field'>
+              <label className='label'>Username</label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 

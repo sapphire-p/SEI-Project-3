d00c2d389ea41f088f3d4f6c37ed9716279240a6
kumarmehta019 <kumarmehta019@gmail.com>
2021-11-12 16:43:09 +0000

    adding authentication

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useHistory, useLocation } from 'react-router-dom'
+import { getPayload } from './helpers/auth'
 import Logo from '../AssetsTest/logo3.png'
 
 
 
 const NavBar = () => {
-  // const history = useHistory()
-  // const location = useLocation() 
+  const history = useHistory()
+  const location = useLocation()
 
-  // useEffect(() =>{
-    
-  // }, [location.pathname])
+  useEffect(() => {
 
-  // const userIsAuthenticated = () => {
-  //   const payload = getPayload()
-  //   if (!payload) return false
-  //   const now = Math.round(Date.now() / 1000)
-  //   return now < payload.exp
-  // }
+  }, [location.pathname])
 
-  // const handleLogout = () => {
-  //   window.localStorage.removeItem('token')
-  //   history.push('/')
-  // }
+  const userIsAuthenticated = () => {
+    const payload = getPayload()
+    if (!payload) return false
+    const now = Math.round(Date.now() / 1000)
+    return now < payload.exp
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('token')
+    history.push('/')
+  }
 
   return (
     <nav className="navbar is-black is-fixed-top">
@@ -39,22 +40,21 @@ const NavBar = () => {
             <Link to="/museums" className="link is-size-6 has-text-weight-light"><i className="far fa-eye"></i> All Museums</Link>
           </div>
           <div className="navbar-item">
-            <Link to="/museums" className="link is-size-6 has-text-weight-light"><i className="fas fa-map-marked-alt"></i> Museums Map</Link>
+            {userIsAuthenticated() && <Link to="/museumsmap" className="link is-size-6 has-text-weight-light"><i className="fas fa-map-marked-alt"></i> Museums Map</Link>}
           </div>
         </div>
         <div className="navbar-end">
-          <div className="navbar-item">
-            <Link to="/register" className="link is-size-6 has-text-weight-light"><i className="fas fa-clipboard-check"></i> Register</Link>
-          </div>
-          <div className="navbar-item">
-            <Link to="/login" className="link is-size-6 has-text-weight-light"><i className="fas fa-sign-in-alt"></i> Login</Link>
-          </div>
-          <div className="navbar-item">
-            <Link to="/" className="link is-size-6 has-text-weight-light"><i className="fas fa-user"></i> Profile</Link>
-          </div>
-          <div className="navbar-item">
-            <Link to="/login" className="link is-size-6 has-text-weight-light"><i className="fas fa-sign-out-alt"></i> Logout</Link>
-          </div>
+          {!userIsAuthenticated() ?
+            <>
+              <div className="navbar-item"><Link to="/register" className="link is-size-6 has-text-weight-light"><i className="fas fa-clipboard-check"></i> Register</Link></div>
+              <div className="navbar-item"><Link to="/login" className="link is-size-6 has-text-weight-light"><i className="fas fa-sign-in-alt"></i> Login</Link></div>
+            </>
+            :
+            <>
+              <div className="navbar-item"><Link to="/profile" className="link is-size-6 has-text-weight-light"><i className="fas fa-user"></i> Profile</Link></div>
+              <div className="navbar-item"><a className="link is-size-6 has-text-weight-light" onClick={handleLogout}><i className="fas fa-sign-out-alt"></i> Logout</a></div>
+            </>
+          }
         </div>
       </div>
     </nav>

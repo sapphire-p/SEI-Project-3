e284eda42620182d8ac54778c9a02ea26c30d0a1
olys6 <oliver.skjevesland@gmail.com>
2021-11-12 16:25:29 +0100

    animation

--- front-end/src/components/FeaturedMuseum.js
+++ front-end/src/components/FeaturedMuseum.js
@@ -1,33 +1,29 @@
 import React, { useState, useEffect } from 'react'
 // import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import ScrollAnimation from 'react-animate-on-scroll'
+
+
 
 const FeaturedMuseum = () => {
 
-  // const [randomMuseum, setRandomMuseum] = useState([])
   const [featuredMuseums, setFeaturedMuseums] = useState([])
-  // const location = useLocation()
-  // const { id } = useParams()
 
-  // useEffect(() => {
-  //   try {
-  //     const { data1 } = await axios.get(`/api/museums/[${Math.floor(Math.random() * 31).toString()}]`)
-  //     setRandomMuseum(data1)
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  // }, [])
-
-  // useEffect(() => {
-
-  // }, [location.pathname])
+  // const Animation = () => {
+  //   return (
+  //     <ScrollAnimation animateIn="fadeIn">
+  //       <h1>TESTING SCROLL</h1>
+  //     </ScrollAnimation>
+  //   )
+  // }
 
   useEffect(() => {
 
     const getData = async () => {
       try {
         const { data } = await axios.get('/api/museums')
-        console.log(data)
+        // console.log(data)
         // setFeaturedMuseums(data[2])
         setFeaturedMuseums(data[Math.floor(Math.random() * 31)])
         // console.log('ONE OF THE MUSEUMS ->', featuredMuseums)
@@ -41,16 +37,46 @@ const FeaturedMuseum = () => {
 
 
   return (
-    <section className="hero is-black is-fullheight">
+    <section className="hero is-black is-halfheight">
+      <ScrollAnimation animateIn="bounceInRight">
+        <h2 className="title has-text-centered is-size-1 featuredMuseumTitle">Featured Museum</h2>
+      </ScrollAnimation>
+      <p className="has-text-centered arrowBelowTitle"><i className="fas fa-angle-down"></i></p>
       <div className="hero-body">
         <div className="columns container is-full">
           <div className="column is-half custom-flex ">
-            <h1 className="title hero-title mb-6 has-text-centered is-size-2">{featuredMuseums.name}</h1>
+            <ScrollAnimation animateIn="fadeIn">
+              <h1 className="title hero-title mb-6 has-text-centered is-size-2">
+                {featuredMuseums.name}
+              </h1>
+            </ScrollAnimation>
             <p className="subtitle hero-subtitle">{featuredMuseums.description}</p>
             {/* <p className="subtitle hero-subtitle">Location: <strong>{featuredMuseums.region}</strong></p>
             <p className="subtitle hero-subtitle">Date established: <strong>{featuredMuseums.date_established}</strong></p>
             <p className="subtitle hero-subtitle">Address: <strong>{featuredMuseums.address}</strong></p> */}
-            <a href={featuredMuseums.website} target="_blank" rel="noreferrer" className="button is-white is-fullwidth visitWebiteBtn is-large"><i className="fas fa-info-circle"></i> More Info </a>
+            <Link to={`/museums/${featuredMuseums.id}`}>
+              <a target="_blank" rel="noreferrer" className="button is-white is-fullwidth visitWebiteBtn is-large">
+                <i className="fas fa-info-circle">
+                </i> More Info
+              </a>
+            </Link>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p>
+            <p>more stuff</p><p>more stuff</p>
+            <p>more stuff</p>
           </div>
           <div className="column is-half show-size ">
             <img className="heroImageHome" src={featuredMuseums.image}/>

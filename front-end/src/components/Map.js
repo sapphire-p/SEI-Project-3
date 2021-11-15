ed46b748c30951d3a211cc0125fc23854ff80ed1
Sapphire Paston <sapphire.paston@gmail.com>
2021-11-15 19:45:41 +0000

    axios GET All Museums request set up in Map.js

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 import ReactMapGL, { Marker, Popup } from 'react-map-gl'
 // import ReactMapGL, { Marker } from 'react-map-gl'
 import locationData from '../data/locations'
+import axios from 'axios'
+
+
 
 const Map = () => {
 
   const [userLocation, setUserLocation] = useState(null)
 
+  // Can set viewPort to { latitude: 51.509240, longitude: 0.005540 } initially if need be for testing
   const [viewPort, setViewPort] = useState({
-
     latitude: 51.509240,
     longitude: 0.005540,
     zoom: 7
@@ -18,20 +21,41 @@ const Map = () => {
 
   const [userLocationClicked, setUserLocationClicked] = useState(false)
 
+  // all museums returned from axios GET request
+  const [allMuseums, setAllMuseums] = useState(null)
 
-  // Set viewPort to { latitude: 51.509240, longitude: 0.005540 } initially if need be for testing
+  // error handling for axios GET request
+  const [hasError, setHasError] = useState(false)
+
+
+
 
   useEffect(() => {
+
     window.navigator.geolocation.getCurrentPosition(position => {
       console.log('Position ->', position)
       const { latitude, longitude } = position.coords
       setUserLocation({ latitude, longitude })
     })
+
+    const getAllMuseumsData = async () => {
+      try {
+        const { data } = await axios.get('/api/museums')
+        setAllMuseums(data)
+      } catch (err) {
+        console.log(err)
+        setHasError(true)
+      }
+    }
+    getAllMuseumsData()
+
   }, [])
 
   // console.log('viewPort ->', viewPort)
   // console.log('userLocation ->', userLocation)
   console.log('popup ->', popup)
+  console.log('allMuseums ->', allMuseums)
+  console.log('hasError ->', hasError)
 
 
 

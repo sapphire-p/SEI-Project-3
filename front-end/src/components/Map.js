8b9c46442f014149d8f0f73fd14be60ddb6e49cc
Sapphire Paston <sapphire.paston@gmail.com>
2021-11-16 16:30:16 +0000

    Columns set up for map and museums list on the side, and the museums list made scrollable using CSS 'overflow: y auto' proprty

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -30,13 +30,12 @@ const Map = () => {
 
 
 
-
   useEffect(() => {
 
     window.navigator.geolocation.getCurrentPosition(position => {
       console.log('Position ->', position)
       const { latitude, longitude } = position.coords
-      setUserLocation({ latitude, longitude })
+      setUserLocation({ latitude, longitude, zoom: 14 })
     })
 
     const getAllMuseumsData = async () => {
@@ -52,16 +51,182 @@ const Map = () => {
 
   }, [])
 
+
   // console.log('viewPort ->', viewPort)
-  // console.log('userLocation ->', userLocation)
+  console.log('userLocation ->', userLocation)
   // console.log('popup ->', popup)
   // console.log('allMuseums ->', allMuseums)
-  console.log('hasError ->', hasError)
+  // console.log('hasError ->', hasError)
 
 
 
   return (
-    <div className='map-container'>
+
+    <section>
+      <div className='columns map-columns p-3'>
+
+        <div className='column is-four-fifths-desktop is-three-quarters-tablet is-full-mobile has-background-grey-lighter map-container'>
+          {viewPort && userLocation && allMuseums ?
+            <ReactMapGL
+              mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+              height='100%'
+              width='100%'
+              mapStyle='mapbox://styles/mapbox/navigation-night-v1'
+              {...viewPort}
+              onViewStateChange={viewport => setViewPort(viewport)}
+            >
+              <Marker latitude={userLocation.latitude} longitude={userLocation.longitude}>
+                <span onClick={() => setUserLocationClicked(true)}>🔴</span>
+              </Marker>
+              {userLocationClicked &&
+                <Popup
+                  latitude={userLocation.latitude}
+                  longitude={userLocation.longitude}
+                  closeOnClick={true}
+                  onClose={() => setUserLocationClicked(false)}
+                >
+                  <div className='is-size-6 has-text-weight-bold'>
+                    Your location
+                  </div>
+                </Popup>
+              }
+              {allMuseums.map(museum => {
+                return (
+                  <Marker key={museum.location_id} latitude={museum.latitude} longitude={museum.longitude}>
+                    <span onClick={() => setPopup(museum)}>
+                      🏛
+                    </span>
+                  </Marker>
+                )
+              })}
+              {popup &&
+                <Popup
+                  latitude={popup.latitude}
+                  longitude={popup.longitude}
+                  closeOnClick={true}
+                  onClose={() => setPopup(null)}
+                >
+                  <div id='map-card-container' className='p-1'>
+                    <div className='card'>
+                      <div className='card-header'>
+                        <div className='card-header-title cardTitle is-size-7'>{popup.name}</div>
+                      </div>
+                      <Link to={`/museums/${popup._id}`}>
+                        <div className='card-image'>
+                          <figure className='image is-4by3'>
+                            <img src={popup.image} alt={popup.name} />
+                          </figure>
+                        </div>
+                      </Link>
+                      <div className='card-content p-2'>
+                        <h4 className='is-size-7 cardRegion'>{popup.region}</h4>
+                      </div>
+                    </div>
+                  </div>
+                </Popup>
+              }
+            </ReactMapGL>
+            :
+            <h1>{hasError ? 'Oops! Something went wrong when loading the map' : 'Loading...'}</h1>
+          }
+        </div>
+
+        <div className='column map-list-container has-background-grey-light'>
+          {allMuseums ?
+            // <div>List of Museums</div>
+            <div>
+              {
+                allMuseums.map(museum => {
+                  return (
+                    <div key={museum._id} className='p-1'>
+                      <div className='card'>
+                        <div className='card-header'>
+                          <div className='card-header-title cardTitle is-size-7'>{museum.name}</div>
+                        </div>
+                        <div className='card-image'>
+                          <figure className='image is-4by3'>
+                            <img src={museum.image} alt={museum.name} />
+                          </figure>
+                        </div>
+                        <div className='card-content p-2'>
+                          <h4 className='is-size-7 cardRegion'>{museum.region}</h4>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            :
+            <h1>{hasError ? 'Oops! Something went wrong when loading the list of museums' : 'Loading...'}</h1>
+          }
+        </div>
+
+      </div>
+    </section>
+
+  )
+
+}
+
+export default Map
+
+
+
+{/* <Link to={`/museums/${_id}`}>
+<div className='card-image'>
+  <figure className='image is-1'>
+    <img src={image} alt={`Picture of ${name}`} />
+  </figure>
+</div>
+<div className='card-content p-2'>
+  <h4 className='is-size-7 cardRegion'>{region}</h4>
+</div>
+</Link> */}
+
+
+//   < div className = 'card' >
+// <div className='card-header'>
+//   <div className='card-header-title cardTitle is-size-7'>{popup.name}</div>
+// </div>
+//   <div className='card-image'>
+//     <figure className='image is-1'>
+//       <img src={popup.image} alt={popup.name} />
+//     </figure>
+//   </div>
+//   <div className='card-content p-2'>
+//     <h4 className='is-size-7 cardRegion'>{popup.region}</h4>
+//   </div>
+// </div >
+
+
+
+// {locationData.map(location => {
+//   return (
+//     <Marker key={location.id} latitude={location.latitude} longitude={location.longitude}>
+//       <span onClick={() => setPopup(location)}>
+//         {location.icon}
+//       </span>
+//     </Marker>
+//   )
+// })}
+// {popup &&
+//   <Popup
+//     latitude={popup.latitude}
+//     longitude={popup.longitude}
+//     closeOnClick={true}
+//     onClose={() => setPopup(null)}
+//   >
+//     <div>
+//       {popup.name}
+//     </div>
+//   </Popup>
+// }
+
+
+// THIS WORKS WELL - RETURN CONTENTS FROM BEFORE I ADDED 2 COLUMNS
+
+{/* <div className='map-container'>
       {viewPort && userLocation && allMuseums ?
         <ReactMapGL
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
@@ -127,61 +292,4 @@ const Map = () => {
         :
         <h1>Loading your location...</h1>
       }
-    </div>
-  )
-
-}
-
-export default Map
-
-
-
-{/* <Link to={`/museums/${_id}`}>
-<div className='card-image'>
-  <figure className='image is-1'>
-    <img src={image} alt={`Picture of ${name}`} />
-  </figure>
-</div>
-<div className='card-content p-2'>
-  <h4 className='is-size-7 cardRegion'>{region}</h4>
-</div>
-</Link> */}
-
-
-//   < div className = 'card' >
-// <div className='card-header'>
-//   <div className='card-header-title cardTitle is-size-7'>{popup.name}</div>
-// </div>
-//   <div className='card-image'>
-//     <figure className='image is-1'>
-//       <img src={popup.image} alt={popup.name} />
-//     </figure>
-//   </div>
-//   <div className='card-content p-2'>
-//     <h4 className='is-size-7 cardRegion'>{popup.region}</h4>
-//   </div>
-// </div >
-
-
-
-// {locationData.map(location => {
-//   return (
-//     <Marker key={location.id} latitude={location.latitude} longitude={location.longitude}>
-//       <span onClick={() => setPopup(location)}>
-//         {location.icon}
-//       </span>
-//     </Marker>
-//   )
-// })}
-// {popup &&
-//   <Popup
-//     latitude={popup.latitude}
-//     longitude={popup.longitude}
-//     closeOnClick={true}
-//     onClose={() => setPopup(null)}
-//   >
-//     <div>
-//       {popup.name}
-//     </div>
-//   </Popup>
-// }
+    </div> */}

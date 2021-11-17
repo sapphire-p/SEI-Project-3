827ac12b6ec4075c2682520af236fdda9bcf7019
Sapphire Paston <sapphire.paston@gmail.com>
2021-11-17 16:22:34 +0000

    merging

--- front-end/src/components/MuseumCard.js
+++ front-end/src/components/MuseumCard.js
@@ -13,30 +13,11 @@ const MuseumCard = ({ _id, name, image, region }) => {
   // }
 
   return (
-<<<<<<< HEAD
-    // <div key={_id}  className='column is-one-quarter-desktop animate__animated animate__faster  museumCard'>
+
     <div className='card'>
       <div className='card-header is-flex is-align-items-center'>
         <div className='card-header-title cardTitle is-size-7'>{name}</div>
         <AddingDeletingToFavourites id={_id} />
-=======
-    <div key={_id}  className='column is-one-quarter-desktop animate__animated animate__faster  museumCard'>
-      <div className='card'>
-        <div className='card-header'>
-          <div className='card-header-title cardTitle is-size-7'>{name}</div>
-          <AddingDeletingToFavourites id={_id}/> 
-        </div>
-        <Link to={`/museums/${_id}`}>
-          <div className='card-image'>
-            <figure className='image is-1'>
-              <img src={image} alt={`Picture of ${name}`} className='museumImages'/>
-            </figure>
-          </div>
-          <div className='card-content p-2'>
-            <h4 className='is-size-7 cardRegion'>{region}</h4>
-          </div>
-        </Link>
->>>>>>> development
       </div>
       <Link to={`/museums/${_id}`}>
         <div className='card-image'>
@@ -50,7 +31,6 @@ const MuseumCard = ({ _id, name, image, region }) => {
       </Link>
     </div>
 
-  // </div>
   )
 
 }

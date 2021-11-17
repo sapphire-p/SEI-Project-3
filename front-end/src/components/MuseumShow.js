2b01db4762322a1f9daedef06d72718efae8e814
olys6 <oliver.skjevesland@gmail.com>
2021-11-17 16:16:54 +0100

    backend for exhibits done and a tiny but more styling done

--- front-end/src/components/MuseumShow.js
+++ front-end/src/components/MuseumShow.js
@@ -102,7 +102,9 @@ const MuseumShow = () => {
               </div>
               <p className='subtitle has-text-right has-text-white'>
                 {/* <a onClick={handleClick} className="bookmark far animate__animated animate__faster fa-bookmark" id={id}></a> */}
-                <AddingDeletingToFavourites />
+                <div className='bookmarkStyling'>
+                  <AddingDeletingToFavourites />
+                </div>
                 <hr />
                 Region: {museum.region}
                 <br />
@@ -169,6 +171,25 @@ const MuseumShow = () => {
                       dots={true}
                       slideImageFit='cover'
                     />
+                  </div>
+                  <div className='column is-half'>
+                    <div className='card'>
+                      <header className='card-header'>
+                        <p className='card-header-title has-text-centered is-flex is-justify-content-center standoutExhibitHeader'>
+                          {museum.exhibits_name}
+                        </p>
+                      </header>
+                      <div className='card-image'>
+                        <figure className='image is-1'>
+                          <img src={museum.exhibits_image} className='exhibitImage'/>
+                        </figure>
+                      </div>
+                      <div className='card-content'>
+                        <div className='content'>
+                          {museum.exhibits_description}
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </section>
